60f08e40418d713c7348f46a84438c328f4b385d
shashankshekhar228 <sshekharsupermanreturnsagain@gmail.com>
2022-11-25 01:00:53 +0530

    The initial assigment with edited jenkins file

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -9,19 +9,19 @@ pipeline {
         stage('Build Code') {
             steps {
                 sh "chmod u+x addition.py"
-                sh "./addition.py"
+                sh "python3 addition.py"
             }
         }
         stage('Test Code') {
             steps {
                 sh "chmod u+x test1.py"
-                sh "./test1.py"
+                sh "python3 test1.py"
             }
         }
         stage('Test Code') {
             steps {
                 sh "chmod u+x test2.py"
-                sh "./test2.py"
+                sh "python3 test2.py"
             }
         }
     } 
